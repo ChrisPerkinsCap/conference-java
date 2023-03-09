ab16e4b2b65a1eb53573570d8e3e558e6aa692bb
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-03-09 09:40:11 +0100

    Deleted the app. to move it into the pagage org.pluralsight.java

--- src/main/java/org/pluralsight/repository/SpeakerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.pluralsight.repository;
-
-import org.pluralsight.model.Speaker;
-
-import java.util.List;
-
-public interface SpeakerRepository {
-    List<Speaker> findAll();
-}
--- src/main/java/org/pluralsight/service/SpeakerService.java
+++
@@ -1,9 +0,0 @@
-package org.pluralsight.service;
-
-import org.pluralsight.model.Speaker;
-
-import java.util.List;
-
-public interface SpeakerService {
-    List<Speaker> findAll();
-}
